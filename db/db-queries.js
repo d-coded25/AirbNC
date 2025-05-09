7ba777234e7f176cae9b6a47d8dfccc64c982cfb
PETER STANBANK <peterstanbank@Apple-MBP---PETER.local>
2025-05-09 16:29:37 +0100

    Add Drop Queries

--- db/db-queries.js
+++ db/db-queries.js
@@ -1,10 +1,16 @@
+// Drop database tables:
+const dropPropertyTypes = 'DROP TABLE property_types;';
+const dropUsers = 'DROP TABLE users;';
+const dropProperties = 'DROP TABLE properties;';
+const dropReviews = 'DROP TABLE reviews;';
+
 // Create database tables queries:
-const propertyTypes = `CREATE TABLE property_types (
+const createPropertyTypes = `CREATE TABLE property_types (
 property_type VARCHAR NOT NULL PRIMARY KEY, 
 description TEXT NOT NULL
 );`;
 
-const users = `CREATE TABLE users (
+const createUsers = `CREATE TABLE users (
 user_id SERIAL PRIMARY KEY,
 first_name VARCHAR NOT NULL,
 surname VARCHAR NOT NULL,
@@ -12,10 +18,10 @@ email VARCHAR NOT NULL,
 phone_number VARCHAR,
 is_host BOOLEAN NOT NULL,
 avatar VARCHAR,
-created_at TIMESTAMP default
+created_at TIMESTAMP default NOW()
 );`;
 
-const properties = `CREATE TABLE properties (
+const createProperties = `CREATE TABLE properties (
 property_id SERIAL PRIMARY KEY,
 host_id INT NOT NULL,
 name VARCHAR NOT NULL,
@@ -23,18 +29,27 @@ location VARCHAR NOT NULL,
 property_type VARCHAR NOT NULL REFERENCES property_types(property_type),
 price_per_night decimal NOT NULL,
 description TEXT
-)`;
+);`;
 
-const reviews = `CREATE TABLE reviews (
+const createReviews = `CREATE TABLE reviews (
 review_id SERIAL PRIMARY KEY,
 property_id INT NOT NULL REFERENCES properties(property_id),
 guest_id INT NOT NULL REFERENCES users(user_id),
 rating INT NOT NULL,
 comment TEXT,
-created_at TIMESTAMP default
-)`;
+created_at TIMESTAMP default NOW()
+);`;
 
 // Insert database data queries:
 
 // Export database queries:
-module.exports = { propertyTypes, users, properties, reviews };
+module.exports = {
+  dropPropertyTypes,
+  dropUsers,
+  dropProperties,
+  dropReviews,
+  createPropertyTypes,
+  createUsers,
+  createProperties,
+  createReviews,
+};
