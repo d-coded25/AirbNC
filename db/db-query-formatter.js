aa0fe80df6ead30771071410ecd69f0535710bfc
PETER STANBANK <peterstanbank@Apple-MBP---PETER.local>
2025-05-11 21:46:05 +0100

    Add User ID Lookup And Properties Formatter Function

--- db/db-query-formatter.js
+++ db/db-query-formatter.js
@@ -22,8 +22,39 @@ const formatUsers = function (usersData) {
   return formattedUsers;
 };
 
+// Lookups:
+const createUsersIdLookUp = function (usersData) {
+  const usersLookUp = {};
+
+  usersData.rows.forEach((user) => {
+    const fullName = `${user.first_name} ${user.surname}`;
+    usersLookUp[fullName] = user.user_id;
+  });
+
+  return usersLookUp;
+};
+
+// Properties formatter:
+const formatProperties = function (propertiesData, usersData) {
+  const usersIdLookup = createUsersIdLookUp(usersData);
+
+  const formattedProperties = propertiesData.map((property) => {
+    return [
+      usersIdLookup[property.host_name],
+      property.name,
+      property.location,
+      property.property_type,
+      property.price_per_night,
+      property.description,
+    ];
+  });
+
+  return formattedProperties;
+};
+
 // Export format data function:
 module.exports = {
   formatPropertyTypes,
   formatUsers,
+  formatProperties,
 };
