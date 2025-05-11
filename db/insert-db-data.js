21442b1d2f2977a4c1ea0be35649d0910e349b5a
PETER STANBANK <peterstanbank@Apple-MBP---PETER.local>
2025-05-11 22:20:31 +0100

    Add Insert Data Functions

--- db/insert-db-data.js
+++ db/insert-db-data.js
@@ -1,8 +1,25 @@
 // Import database connections:
 const db = require('./db-connections');
 
+// Import postgres format:
+const format = require('pg-format');
+
 // Import data queries:
-// const {} = require('./db-queries');
+const {
+  insertPropertyTypesQuery,
+  insertUsersQuery,
+  insertPropertiesQuery,
+  insertReviewsQuery,
+} = require('./db-queries');
+console.log();
+
+// Import format data function:
+const {
+  formatPropertyTypes,
+  formatUsers,
+  formatProperties,
+  formatReviews,
+} = require('./db-query-formatter');
 
 // Database insert data:
 const insertDbData = async function (data) {
@@ -15,6 +32,36 @@ const insertDbData = async function (data) {
     reviewsData,
     usersData,
   } = data;
+
+  try {
+    const propertyTypesResponse = await db.query(
+      format(insertPropertyTypesQuery, formatPropertyTypes(propertyTypesData))
+    );
+    const usersResponse = await db.query(
+      format(insertUsersQuery, formatUsers(usersData))
+    );
+    const propertiesResponse = await db.query(
+      format(
+        insertPropertiesQuery,
+        formatProperties(propertiesData, usersResponse)
+      )
+    );
+    const reviewsResponse = await db.query(
+      format(
+        insertReviewsQuery,
+        formatReviews(reviewsData, usersResponse, propertiesResponse)
+      )
+    );
+
+    // console.log('Property types response:', propertyTypesResponse.rows);
+    // console.log('Users response:', usersResponse.rows);
+    // console.log('Properties response:', propertiesResponse.rows);
+    // console.log('Reviews response:', reviewsResponse.rows);
+
+    console.log('Resolved Insert Data!');
+  } catch (err) {
+    console.log('Rejected Insert Data:', err.message);
+  }
 };
 
 // Export insert data function:
